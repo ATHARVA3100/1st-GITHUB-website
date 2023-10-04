d83888a3d1e74e4258b583c60623ca720d1efcab
ATHARVA3100 <146477534+ATHARVA3100@users.noreply.github.com>
2023-10-04 09:35:02 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,42 @@
 
+ 
+  
+  var result;
+  
+   function Tables() {
+    a = prompt("Enter n for it's tables")
+      a = Number.parseInt(a)
+      b = prompt("Enter b for extent of tables")
+      b = Number.parseInt(b)
+      for (let i = 1; i <= b; i++) {
+        document.write(a + "X" + i + "=" + a * i + "<Br>")
+      }}
+
+      function Multiplication(){
+     a = prompt("Enter n for it's Multiplication")
+      a = Number.parseInt(a)
+      b = prompt("Enter b for it's Multiplication")
+      b = Number.parseInt(b)
+      document.write(a+" X "+ b +"=" +a * b)}
+      
+    function Remainder(){a = prompt("Enter divident")
+      a = Number.parseInt(a)
+      b = prompt("Enter divisor")
+      b = Number.parseInt(b)
+      document.write(a +" % "+ b +"="+a % b)}
+      
+    function Division(){a = prompt("Enter divident")
+      a = Number.parseInt(a)
+      b = prompt("Enter divisor")
+      b = Number.parseInt(b)
+       document.write(a +" / "+b +"="+a / b)}
+
+  
+    
+  
+
+
+
+
+
+
